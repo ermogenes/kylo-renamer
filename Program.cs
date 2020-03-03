8f6c23b41705b476e29cecd588b9b7f3b66bcb8a
Ermogenes Palacio <ermogenes@gmail.com>
2020-03-03 12:18:04 -0300

    Add barely finished cannonical version

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace kylo_renamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"f:\";
+            List<string> arquivos = Directory.GetFiles(path, "*.mp3").ToList();
+            arquivos = arquivos.OrderBy(a => Guid.NewGuid()).ToList();
+            int qtd = arquivos.Count;
+            Console.WriteLine($"Arquivos .mp3 encontrados: {qtd}. Pressione uma tecla para iniciar...");
+            Console.ReadKey();
+            int parcial = 0;
+            foreach(var arquivo in arquivos)
+            {
+                string nomeNovo = arquivo.Insert(3, Guid.NewGuid().ToString().Substring(0,6) + " ");
+                Console.WriteLine($"#{++parcial} De: [{arquivo}] para [{nomeNovo}]");
+                File.Move(arquivo, nomeNovo);                
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+## Kylo Renamer
+
+A not so good unidimensional file renamer.
+
+---
+
+It's not like you expected. Seriously. So much potencial trashed away.
+
+Maybe in the non-cannon version.
